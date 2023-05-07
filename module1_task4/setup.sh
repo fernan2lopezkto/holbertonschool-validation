40f1e39aab81aa63e668b7c2b4cdea13cf72fc0a
juan_latitude <fernan2lopezkto@gmail.com>
2023-05-07 20:17:04 -0300

    silla

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y make
+sudo apt-get install -y make
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 
-dpkg -i hugo_0.79.0_Linux-64bit.deb
+sudo dpkg -i hugo_0.79.0_Linux-64bit.deb
 
 make build
 
