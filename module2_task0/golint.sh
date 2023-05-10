cd013d03eeba95be3869382df6f4009840aff380
juan_latitude <fernan2lopezkto@gmail.com>
2023-05-10 19:48:01 -0300

    s

---
+++ module2_task0/golint.sh
@@ -0,0 +1,4 @@
+# binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
+
+golangci-lint --version
