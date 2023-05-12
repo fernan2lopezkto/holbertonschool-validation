581e411a98301bbefed49bc27535a800c9e9e319
juan_latitude <fernan2lopezkto@gmail.com>
2023-05-12 10:15:46 -0300

    delete sh

--- module2_task0/golint.sh
+++
@@ -1,4 +0,0 @@
-# binary will be $(go env GOPATH)/bin/golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
-
-golangci-lint --version
